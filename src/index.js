dcef182da8805dc97d9884bc6c1ce77c508a01f2
Rishabh Singh <rishabhcena1@gmail.com>
2020-05-11 02:19:35 +0530

    setting it up for redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import 'normalize.css'
 import './assets/styles/custom.css'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
 
+const store = createStore(reducer)
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const root = document.getElementById('root')
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+, root)
